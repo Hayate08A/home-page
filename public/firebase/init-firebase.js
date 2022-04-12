a9fcd8293c301e0a4e2e2fc1524d6f01564ca95b
Ha08ya <ha08ya@gmail.com>
2022-04-12 12:08:17 +0900

    [WIP] firebase

---
+++ public/firebase/init-firebase.js
@@ -0,0 +1,7 @@
+import { firebase } from "firebase/app";
+import { firebaseConfig } from "./firebase_config";
+
+// TODO: Replace the following with your app's Firebase project configuration
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
