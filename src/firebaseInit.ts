fda5cd3ba68fd5bf15c8747475f3608f36e1d3f2
Ha08ya <ha08ya@gmail.com>
2022-07-20 21:41:12 +0900

    [WIP] .env

--- src/firebaseInit.ts
+++ src/firebaseInit.ts
@@ -1,3 +1,4 @@
+// require("dotenv").config();
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,14 +7,19 @@ import firebase from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCgfO-LxOg_h0lIONX0kzEfpJcxOMJPl1U",
-  authDomain: "myhomepage-b2fe0.firebaseapp.com",
-  projectId: "myhomepage-b2fe0",
-  storageBucket: "myhomepage-b2fe0.appspot.com",
-  messagingSenderId: "401921612366",
-  appId: "1:401921612366:web:c4db936686d94cae7e011f",
-  measurementId: "G-Y1B1DETD7X",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
+
+console.log(
+  "process.env.FIREBASE_MEASUREMENT_I : ",
+  process.env.FIREBASE_MEASUREMENT_ID
+);
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
